98fb447e6bc6f77c2f8930cf8060cb21b4169cb8
Breno Miloch <beno.m.miloch@hotmail.com>
2023-04-21 17:12:21 -0300

    Questão 4 concluída.

--- PythonBrasil/Ex03.py
+++ PythonBrasil/Ex03.py
@@ -5,8 +5,4 @@ Num2 = int(input("Digite o secundo numero: "))
 
 Resultado = Num1 + Num2
 
-print("Sua soma deu: " + Resultado)
-
-#ANOTAÇÃO: A Soma de 10+10 não foi 20 e sim 1010, para corrigir foi necessario adicionar
-# o int()
-#PROBLEMA: Num1= input("Digite o primeiro numero: ") vai ser tratado como string por isso a necessidade de colocar int()
+print("Sua soma deu: ", Resultado)
---
+++ PythonBrasil/Ex04.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+NotaBi_1 = int(input("Digite a primeira nota bimestral: "))
+NotaBi_2 = int(input("Digite a segunda nota bimestral: "))
+NotaBi_3 = int(input("Digite a terceira nota bimestral: "))
+NotaBi_4 = int(input("Digite a quarta nota bimestral: "))
+
+Media = (NotaBi_1 + NotaBi_2 + NotaBi_3 + NotaBi_4)/4
+
+print("A media foi de: ", Media)
