0929a28b8c01adb655424bc2cb47ab88d77741e0
Breno Marques Miloch <breno.miloch@apc.local>
2023-04-22 09:01:47 -0300

    Questão 09 Concluído.

---
+++ PythonBrasil/Ex09.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+# Formula | C = 5 * ((F-32) / 9)
+
+print("Programa para converter Fahrenheit para Celsius.")
+fahrenheit = float(input("Digite o Fahrenheit: "))
+converter = 5* ((fahrenheit-32)/9)
+print("Em celsius é : ",converter)
