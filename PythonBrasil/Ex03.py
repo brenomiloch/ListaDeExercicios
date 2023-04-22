67e10156c4e18bbcbf37f25cdc8f5fbfc61cbe94
Breno Marques Miloch <breno.miloch@apc.local>
2023-04-22 11:07:41 -0300

    Questão 03: Concluída

--- PythonBrasil/Ex03.py
+++ PythonBrasil/Ex03.py
@@ -1,8 +1,7 @@
-# Faça um Programa que peça dois números e imprima a soma.
+# Faça um Programa que peça dois números e imprima a soma;
 
 Num1 = int(input("Digite o primeiro numero: "))
 Num2 = int(input("Digite o secundo numero: "))
-
 Resultado = Num1 + Num2
 
 print("Sua soma deu: ", Resultado)
