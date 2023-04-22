e91a84ec8785915a009fc7c1b5c3567454c89c8e
Breno Miloch <beno.m.miloch@hotmail.com>
2023-04-22 00:17:16 -0300

    Questão 6 concluída

---
+++ PythonBrasil/Ex06.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+pi = 3.14159265
+raio = float(input("Digite a área: "))
+area = pi * raio**2
+print("O valor da área é: ", area ,"cm²")
