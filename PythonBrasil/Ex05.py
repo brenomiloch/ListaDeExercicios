1c97a7017729a7a166ed4927309145fe1bf2b5c8
Breno Miloch <beno.m.miloch@hotmail.com>
2023-04-21 23:41:15 -0300

    Questão 5 concluída.

---
+++ PythonBrasil/Ex05.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+
+Metros = int(input("Metros : "))
+
+Resultado = Metros * 100
+
+print(Metros,"metros equivalem a : ", Resultado, "Centrimetros.")
