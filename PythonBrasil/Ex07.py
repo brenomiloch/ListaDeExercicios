4cc643cba1c6d371d07d1e021e4070577b4423c4
Breno Miloch <beno.m.miloch@hotmail.com>
2023-04-22 00:44:57 -0300

    Questão 07 concluído.

---
+++ PythonBrasil/Ex07.py
@@ -0,0 +1,6 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input("Valor de um dos lados do quadrado: "))
+
+area = (lado ** 2)*2
+print("Área em dobro : ",area)
