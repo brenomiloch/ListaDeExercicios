d0d578048f59a65a57b781b223851de752dbbe22
brenomiloch <breno.m.miloch@hotmail.com>
2023-04-29 12:32:33 -0300

    Adicionado Python Alura

---
+++ PythonAlura/jogo_adivinha_numero.py
@@ -0,0 +1,7 @@
+print("BEM VIDO NO JOGO DE ADIVINHAÇÃO!!")
+chute = int(input("Digite seu chute: "))
+numero_certo = 27
+if chute == numero_certo:
+    print("Acertou!!!!")
+else: 
+    print("Errou!!!")
