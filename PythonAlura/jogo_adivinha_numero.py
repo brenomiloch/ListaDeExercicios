f72427455ab3671d7ec6a12885dcfa10d74df219
brenomiloch <breno.m.miloch@hotmail.com>
2023-04-30 09:52:05 -0300

    Atualização do Jogo de Adivinhação de Números.

--- PythonAlura/jogo_adivinha_numero.py
+++ PythonAlura/jogo_adivinha_numero.py
@@ -3,5 +3,7 @@ chute = int(input("Digite seu chute: "))
 numero_certo = 27
 if chute == numero_certo:
     print("Acertou!!!!")
-else: 
-    print("Errou!!!")
+elif chute > numero_certo: 
+    print("Você errou! O seu chute foi maior que o número secreto.")
+else:
+    print("Você errou! O seu chute foi menor que o número secreto.")
