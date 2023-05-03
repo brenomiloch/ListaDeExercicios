24753d9bf715475804d6c61834ab15849c526841
Breno Miloch <beno.m.miloch@hotmail.com>
2023-05-02 23:35:11 -0300

    Questão 20: Concluído.

---
+++ PythonBrasil/Ex20.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+num = float(input("Digite o número: "))
+
+if num >= 0:
+    print("O número é positivo.")
+else:
+    print("O número é negativo.")
