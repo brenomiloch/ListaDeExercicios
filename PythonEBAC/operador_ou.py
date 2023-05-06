aba84cacf74390a38102961b5e72af52a81a5252
brenomiloch <breno.m.miloch@hotmail.com>
2023-05-06 09:10:32 -0300

    Exemplo sobre uso do operador (| : OU) adicionado.

--- PythonEBAC/operador_ou.py
+++ PythonEBAC/operador_ou.py
@@ -6,3 +6,4 @@ print("A: True | B:False  = ",True | False)
 print("A: True | B:True   = ",True | True)
 # O operador "|" retorna True (verdadeiro) se pelo menos uma das expressões for verdadeira e retorna False (falso)
 # apenas se ambas as expressões forem falsas.
+#
