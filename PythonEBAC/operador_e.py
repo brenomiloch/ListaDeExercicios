b6432919bab0d9e6faf6d0830b98f29be249c6e1
brenomiloch <breno.m.miloch@hotmail.com>
2023-05-06 08:54:47 -0300

    Exemplo sobre o uso do operador ( & :  E ) adicionado.

--- PythonEBAC/operador_e.py
+++ PythonEBAC/operador_e.py
@@ -5,3 +5,4 @@ print("A:False &  B:True   = ",False & True)
 print("A: True &  B:False  = ",True & False)
 print("A: True &  B:True   = ",True & True)
 # O operador "&" retorna True (verdadeiro) somente se ambas as expressões forem verdadeiras. Caso contrário,ele retorna False (falso).
+#   
