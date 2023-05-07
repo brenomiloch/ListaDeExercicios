240f87018c2c9292b1008aa82eecb3084144ba9d
brenomiloch <breno.m.miloch@hotmail.com>
2023-05-07 07:47:15 -0300

    Módulo 1 Concluído.

---
+++ PythonEBAC/exercicio_modulo_01.py
@@ -0,0 +1,31 @@
+#Questão 01:
+print('Olá, mundo.')
+#Questão 02:
+a = 320.52 * 3
+b = 834.47 / 119.21
+c = 15378.12 / 5
+print("A =",a)
+print("B =",int(b))
+print("C =",c)
+#Questão 03:
+cancao = 'roda mundo, roda gigante, roda moinho, roda pião.'
+#Método 1
+metodo_1 = cancao.capitalize()
+print('Método 1 :',metodo_1)
+#Método 2
+metodo_2 = cancao.swapcase()
+print('Método 2 :',metodo_2)
+#Método 3
+metodo_3 = cancao.count('roda')
+print('Método 3 :',metodo_3)
+
+#Questão 04:
+# TRUE o not inverte o valor de A, em caso normal seria False, mas com o not e invertido e se torna TRUE. Nesse caso o & só passaria True se ambos fosse verdadeiros com o not ele 
+# inverte o false pra true no A. 
+a_1 = False
+b_2 = True
+x_3 = not a_1 & b_2
+print(x_3)
+
+# Correção professor:
+# 100% aprovado!
