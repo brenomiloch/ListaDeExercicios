3a84648808c8c640fb111fa8f409d44b73f9e0de
brenomiloch <breno.m.miloch@hotmail.com>
2023-05-10 08:45:06 -0300

    Questão 23: Concluído.

--- PythonBrasil/Ex23.py
+++ PythonBrasil/Ex23.py
@@ -1,9 +1,7 @@
 # Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
-
 # A. A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 # B. A mensagem "Reprovado", se a média for menor do que sete;
 # C. A mensagem "Aprovado com Distinção", se a média for igual a dez.
-
 nota_1 = float(input("Digite a nota 1: "))
 nota_2 = float(input("Digite a nota 2: "))
 media = (nota_1 + nota_2)/2
@@ -11,5 +9,5 @@ if media >= 7:
     print("Aprovado")
 elif media < 7:
     print("Reporvado")
-elif meida == 10:
+elif media == 10:
     print("Aporvado")
